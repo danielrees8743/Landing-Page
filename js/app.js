16645ad85c2ac5ea40e314530c01f6bb66c3c154
Dan Rees <danrees8743@gmail.com>
2022-08-30 00:20:19 +0100

    Added more functionallity and styling to the landing page

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-//* Query Selectors
-const header = document.querySelectorAll('.header');
+//? Query Selectors
+//? Global Variables
+const header = document.getElementById('header');
 const sections = document.querySelectorAll('section');
-const navigation = document.querySelectorAll('.nav ');
+const navigation = document.querySelectorAll('.nav');
 
-//* Global Variables
+let navMenu;
+const panels = document.querySelectorAll('.panel');
 
-//* Functions
+//? Functions
+function removeActiveClasses(nodeList) {
+  nodeList.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
 function createNavigation() {
   return `
-  <ul id="nav" class="nav__items">
+  <ul id="nav__items" class="nav__items">
     <li>
       <a  class="nav-item" href="#team">The Team</a>
     </li>
@@ -21,27 +28,63 @@ function createNavigation() {
     <li>
       <a class="nav-item" href="#peoplesay">What People Say</a>
     </li>
-    <button><a class="button" href="#contact">Contact Us</a></button>
   </ul>`;
 }
 
-//* Event Listeners
+//? Event Listeners
 document.addEventListener('DOMContentLoaded', (e) => {
   e.preventDefault();
+  console.log('DOM Loaded...');
   navigation.forEach((item) => {
     item.insertAdjacentHTML('beforeend', createNavigation());
   });
-  console.log('DOM Loaded...');
+  navMenu = document.querySelectorAll('.nav-item');
 });
 
+//? Smooth Scrolling into sections
 for (i = 0; i < navigation.length; i++) {
   navigation[i].addEventListener('click', (e) => {
     e.preventDefault();
-    console.log(e.target);
     const navItem = e.target;
+    removeActiveClasses(navMenu);
     navItem.classList.add('active');
     document
       .querySelector(navItem.getAttribute('href'))
       .scrollIntoView({ behavior: 'smooth' });
   });
 }
+
+//? Sticky Navigation
+window.onscroll = () => {
+  if (window.pageYOffset >= header.offsetTop) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+};
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses(panels);
+    panel.classList.add('active');
+  });
+});
+
+//? Mark current section
+function makeActive(e) {
+  sections.forEach((section) => {
+    const position = section.getBoundingClientRect();
+
+    if (position.top <= 150 && position.bottom >= 150) {
+      document.querySelector(`[href='#${section.id}']`).classList.add('active');
+      document.querySelectorAll('h2').classList.add('active');
+    } else {
+      document
+        .querySelector(`[href='#${section.id}']`)
+        .classList.remove('active');
+    }
+  });
+}
+document.addEventListener('scroll', (e) => {
+  makeActive(e);
+});
